c393ff146325fd97093a6c9b490b94da55df8e41
Bolu <bolu1920@gmail.com>
2025-09-07 16:46:39 +0100

    ...

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [          
-    path("books/fbv/", list_books, name="book_list_fbv"),   # Function-based view
+    path("books/fbv/", list_books, name="list_books"),   # Function-based view
     path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # Library detail view
 ]
