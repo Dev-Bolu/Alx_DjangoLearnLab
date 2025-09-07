ed165b85e6683286aa56ba273adb9254d9280774
Bolu <bolu1920@gmail.com>
2025-09-07 17:29:30 +0100

    ...

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # relationship_app/urls.py
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Function-based view: list all books
@@ -8,4 +9,9 @@ urlpatterns = [
 
     # Class-based view: details of one library
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    
+    # If you want to customize login/logout views, you can do so here
+    path('account/login/', auth_views.LoginView.as_view(), name='login'),
+    path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
 ]
