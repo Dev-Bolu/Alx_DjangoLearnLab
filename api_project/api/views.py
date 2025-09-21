c07ff880969500bbc4c73e0602c71929c2edb386
Bolu <bolu1920@gmail.com>
2025-09-21 04:00:38 +0100

    first commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,13 +2,23 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]   # ✅ only authenticated users
 
 
 # New ViewSet with full CRUD
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]   # default
+    
+    
+    
+def get_permissions(self):
+        if self.action == 'destroy':  # DELETE
+            return [IsAdminUser()]    # only admins can delete
+        return super().get_permissions()
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'rest_framework.authtoken', # ✅ Add this for token auth
+        
+
+    
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # optional (for browsable API)
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # default: only authenticated users
+    ],
+}
