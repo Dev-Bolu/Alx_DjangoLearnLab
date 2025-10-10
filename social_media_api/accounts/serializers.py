283013f78aa1e1610e2622f72172bc1301ca2658
Bolu <bolu1920@gmail.com>
2025-10-10 13:22:19 +0100

    ...

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework.authtoken.models import Token
 
+User = get_user_model()  # dynamically get the active user model
+
+
+# ------------------ USER REGISTRATION SERIALIZER ------------------
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -10,16 +14,22 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User(
+        """
+        Create a new user using Django's built-in manager,
+        then generate an authentication token.
+        """
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            bio=validated_data.get('bio', '')
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        # create token for user
+        Token.objects.create(user=user)
         return user
 
 
+# ------------------ USER LOGIN SERIALIZER ------------------
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
